a8091a400eb3d0f023e984da89b601df3a8fbd1b
seconoid <seconoid@gmail.com>
2016-02-01 13:03:45 +0900

    新規登録ページを追加

--- WebContent/join.jsp
+++ WebContent/join.jsp
@@ -4,7 +4,21 @@
 
 <!-- main_layout.jspにコンテンツとタイトルを渡す -->
 <c:import url="./layout/main_layout.jsp">
-	<c:param name="title" value=""/>
-	<c:param name="content">
+	<c:param name="title" value="新規登録"/>
+	<c:param name="content">	
+		<form action="InsertServlet" method="post"><br>
+			ﾕｰｻﾞID	：<input type="text" name="id"  required><br>
+			${ idErr }<br>
+			ﾕｰｻﾞ名	：<input type="text" name="name"  required><br>
+			${ nameErr }<br>
+			ﾊﾟｽﾜｰﾄﾞ	：<input type="text" name="password" required><br>
+			${ passErr }<br>
+			ﾒｰﾙｱﾄﾞﾚｽ	：<input type="email" name="mail_adress" required><br>
+			生年月日	：<input type="date" name="birthday"><br>
+			男：<input type="radio" name="sex" value="0" checked>
+			女：<input type="radio" name="sex" value="1"><br>
+			<input type="submit" value="登録"><br>
+			${ mess }
+		</form>
 	</c:param>
 </c:import>
---
+++ src/user/InsertServlet.java
@@ -0,0 +1,83 @@
+package user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InsertServlet
+ */
+@WebServlet("/InsertServlet")
+public class InsertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("password");
+		String mail_adress = request.getParameter("mail_adress");
+		String birthday = request.getParameter("birthday");
+		String sex = request.getParameter("sex");
+		
+		boolean delete_flag = false;
+		
+		boolean  isErr = false;
+		
+		
+		if(id == null ||  id.isEmpty()){
+			request.setAttribute("idErr", "idを入力して");
+			isErr = true;
+		}
+		
+		if(name == null ||  name.isEmpty()){
+			request.setAttribute("nameErr", "nameを入力して");
+			isErr = true;
+		}
+		
+		if(pass == null ||  pass.isEmpty()){
+			request.setAttribute("passErr", "ﾊﾟｽを入力して");
+			isErr = true;
+		}
+		
+		
+		
+		if(!isErr){
+			ZIdolyDao dao = new ZIdolyDao();
+			int count = dao.insert(id, name, pass, mail_adress, birthday, sex, delete_flag);
+			
+			if(count <= 0){
+				request.setAttribute("mes", "データが更新されてない");
+			}else{
+				request.setAttribute("mes", "データを更新しました");
+			}
+		}
+		
+		request.getRequestDispatcher("join.jsp").forward(request, response);
+		
+	}
+
+}
