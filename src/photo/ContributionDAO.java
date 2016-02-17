371adcfb1e548f1c23b113aa6cc21c915a37d727
seconoid <seconoid@gmail.com>
2016-02-17 18:05:11 +0900

    各DAOにプロパティファイル読み込みを追加

--- src/photo/ContributionDAO.java
+++ src/photo/ContributionDAO.java
@@ -5,17 +5,40 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ContributionDAO {
-	private Connection getConnection() 
-			throws ClassNotFoundException,SQLException{
-		Class.forName("com.mysql.jdbc.Driver");
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost/hew2016?useUnicode=true&characterEncoding=utf8",
-				"root",
-				""
-				);
-	}
+	private Connection getConnection()
+			throws ClassNotFoundException
+							, SQLException{
+			
+			//// properties より環境変数を取得する
+			// propertiesより読み込み
+			ResourceBundle bundle = null;
+			try {
+				bundle = ResourceBundle.getBundle("path");
+			}catch (MissingResourceException e) {
+				e.printStackTrace();
+			}
+			// パスを取得
+			String db = bundle.getString("db");
+			String url = "";
+		
+			// 正規表現で抜き取り(""が入り込んでくるため）
+			Pattern p = Pattern.compile("^\"(.+)\"$");
+			Matcher m = p.matcher(db);
+			if (m.find()){
+				System.out.println(m.group(1));
+				url = m.group(1);
+			}
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection(
+					url, "root", "");
+		}
 
 	
 	
