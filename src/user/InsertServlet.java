8be0dc52e0ccacd6887660b0547f09cc549d1b6c
seconoid <seconoid@gmail.com>
2016-02-23 17:13:05 +0900

    新規登録時のユーザ導線を改善

--- src/user/InsertServlet.java
+++ src/user/InsertServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class InsertServlet
@@ -64,7 +65,7 @@ public class InsertServlet extends HttpServlet {
 			isErr = true;
 		}
 		
-		
+		// エラーが無かったら
 		if(!isErr){
 			UserDao dao = new UserDao();
 			int count = dao.insert(id, name, hash, mail_adress, birthday, sex, delete_flag);
@@ -72,12 +73,15 @@ public class InsertServlet extends HttpServlet {
 			if(count <= 0){
 				request.setAttribute("mes", "データが更新されてない");
 			}else{
+				// セッションに登録
+				User user = dao.select(id, hash);
+				HttpSession session = request.getSession();
+				session.setAttribute("user", user);
+				
+				// マイページに遷移
 				request.setAttribute("mes", "データを更新しました");
+				request.getRequestDispatcher("mypage.jsp").forward(request, response);
 			}
 		}
-		
-		request.getRequestDispatcher("join.jsp").forward(request, response);
-		
 	}
-
 }
