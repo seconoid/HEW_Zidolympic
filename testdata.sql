60f1e2d9e448ba95c221a859318ae0eb30d58f70
StreetWorly <yusuke.kumanabe@hotmail.co.jp>
2016-03-01 18:00:53 +0900

    asd

--- testdata.sql
+++ testdata.sql
@@ -1,8 +1,9 @@
 -- テストデータ
 -- 競技テーブル
-insert into Competition values(1, "トライジドリング", 150, "3枚撮る");
+
+insert into Competition values(1, 'トライジドリング', 150, '3枚撮る');
 insert into Competition values(2, "シンクロジドリング", 100, "お題に沿って撮る");
-insert into Competition values(3, "スプラトゥーチ", 50, "光って面積を取れ");
+insert into Competition values(3, 'スプラトゥーチ', 50, '光って面積を取れ');
 
 -- タイトルテーブル
 -- 1XX で トライ, 2XX で シンクロ用タイトル
@@ -32,14 +33,15 @@ insert into title values(203, 2, "職業");
 -- 管理者テーブル
 
 insert into admin (admin_id, name, password, delete_flag) values ("suto", "すとう", "40721", 0);
+insert into admin (admin_id, name, password, delete_flag) values ("mikan", "みかん", "45006", 0);
+insert into admin (admin_id, name, password, delete_flag) values ("secon", "secon", "0416", 0);
+
 
 -- 利用者テーブル
-insert into member values(1, "secon", "secon", "mogemoge", "secon@example.com", 19930416, "m", 0);
+insert into member values(3, "secon", "secon", "mogemoge", "secon@example.com", 19930416, "m", 0);
 insert into member values(2, "mikan", "mikan", "45006", "mikan@gmail", 1992-09-25, "w", 0);
 insert into member values(3, "suto", "suto", "40721", "suto@gmail", 1992-01-29, "m", 0);
 
 -- 画像詳細テーブル
 insert into contribution_details(contribution_id,title_id,img_pass,img_title) value(1, 1, "test0.png", "ほんだつばさ");
 
-insert into admin (admin_id, name, password, delete_flag) values ("mikan", "みかん", "45006", 0);
-insert into admin (admin_id, name, password, delete_flag) values ("secon", "secon", "0416", 0);
