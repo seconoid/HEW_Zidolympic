8e124c80b02e17cea269c37b836923db48c1b140
shelty-mikan <the.truth.of.lie@i.softbank.jp>
2016-03-06 18:27:30 +0900

    ランキングページ完成

--- WebContent/ranking.jsp
+++ WebContent/ranking.jsp
@@ -1,10 +1,50 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-
+<link rel="stylesheet" href="/HEW_Sample/dist/css/lightbox.min.css">
 <!-- main_layout.jspにコンテンツとタイトルを渡す -->
 <c:import url="./layout/main_layout.jsp">
 	<c:param name="title" value=""/>
 	<c:param name="content">
+	
+	
+	<div>
+	<h1>${trylist[0].comname}</h1>
+	<c:forEach items="${trylist}" var="t">
+			<a class="example-image-link" href="/HEW_Zidolympic/UploadImages/${t.img_pass}"
+				data-lightbox="example-1"> <img class="example-image"
+				src="/HEW_Zidolympic/UploadImages/${t.img_pass}" alt="image-1" /></a>
+	</c:forEach>
+	</div>
+	
+	<div>
+	<h1>${synchrolist[0].comname}</h1>
+	<c:forEach items="${synchrolist}" var="s">
+			<a class="example-image-link" href="/HEW_Zidolympic/UploadImages/${s.img_pass}"
+				data-lightbox="example-1"> <img class="example-image"
+				src="/HEW_Zidolympic/UploadImages/${s.img_pass}" alt="image-1" /></a>
+	</c:forEach>
+	</div>
+	
+	<div>
+	<h1>${splatorchlist[0].comname}</h1>
+	<c:forEach items="${splatorchlist}" var="d">
+			<a class="example-image-link" href="/HEW_Zidolympic/UploadImages/${d.img_pass}"
+				data-lightbox="example-1"> <img class="example-image"
+				src="/HEW_Zidolympic/UploadImages/${d.img_pass}" alt="image-1" /></a>
+	</c:forEach>
+	</div>
+	
+	
+	
+	
+	
+	
+	
+		<script src="/HEW_Zidolympic/dist/js/lightbox-plus-jquery.min.js"></script>
+	
+	
+	
+	
 	</c:param>
 </c:import>
---
+++ src/photo/RankingServlet.java
@@ -0,0 +1,53 @@
+package photo;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RankingServlet
+ */
+@WebServlet("/RankingServlet")
+public class RankingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RankingServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf8");
+		
+		RankingDAO dao=new RankingDAO();
+		List<Competition> trylist=dao.try_select();
+		request.setAttribute("trylist", trylist);
+		List<Competition> synchrolist=dao.synchro_select();
+		request.setAttribute("synchrolist", synchrolist);
+		List<Competition> splatorchlist=dao.splatorch_select();
+		request.setAttribute("splatorchlist", splatorchlist);
+		request.getRequestDispatcher("ranking.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	}
+
+}
